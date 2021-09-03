fd886b47d57c287f3551d37e12d5940f866626fb
Literally Collective <bluedragon.cstar@outlook.com>
2021-09-03 06:44:02 -0300

    Feat: update panel

--- src/widgets/Menu/Panel.tsx
+++ src/widgets/Menu/Panel.tsx
@@ -4,6 +4,7 @@ import PanelBody from "./PanelBody";
 import PanelFooter from "./PanelFooter";
 import { SIDEBAR_WIDTH_REDUCED, SIDEBAR_WIDTH_FULL } from "./config";
 import { PanelProps, PushedProps } from "./types";
+import RugDocAuditButton from "./RugDocAuditButton";
 
 interface Props extends PanelProps, PushedProps {
   showMenu: boolean;
@@ -40,7 +41,7 @@ const Panel: React.FC<Props> = (props) => {
   return (
     <StyledPanel isPushed={isPushed} showMenu={showMenu}>
       <PanelBody {...props} />
-
+      {rugDocLink && <RugDocAuditButton auditLink={rugDocLink} />}
       <PanelFooter {...props} />
     </StyledPanel>
   );
